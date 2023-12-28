04f68b68d0f1eb9ab224d69cda8d9de9f11b0671
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-28 10:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function customSort(a,b){
+	
+	const removeIgnoredWords = (str) => str.replace(/(^|\s)(a|an|the)(\s|$)/ig, '').toLowerCase();
+
+	const titleA = removeIgnoredWords(a);
+	const titleB = removeIgnoredWords(b);
+
+	if(titleA < titleB)
+	{
+		return -1;
+	}else if(titleA > titleB)
+	{
+		return 1;
+	}else{
+		return 0;
+	}
+
+}
+
+	const sortedBands = bands.sort(customSort);
+
+    const ulElement = document.createElement('ul');
+	ulElement.id = 'band';
+
+	sortedBands.forEach(band => {
+
+		const liElement = document.createElement('li');
+		liElement.textContent = band;
+		ulElement.appendChild(liElement);
+	});
+ulElement.style.margin = 'auto';
+ulElement.style.width = '50%';
+
+document.body.appendChild(ulElement);
